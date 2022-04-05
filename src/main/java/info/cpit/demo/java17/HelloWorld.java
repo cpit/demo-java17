0cdfa749c5278edc4f09b76522a365df1a1da016
Chris <chris.peters@gebit.de>
2022-04-05 10:10:45 +0200

    HelloWorld and project init

---
+++ src/main/java/info/cpit/demo/java17/HelloWorld.java
@@ -0,0 +1,9 @@
+package info.cpit.demo.java17;
+
+public class HelloWorld {
+	
+	public static void main (String ... args) {
+		System.out.println("Java version: "+ System.getProperty("java.version"));
+	}
+
+}
